1d4e5ed2f60811571b95194c8a208e51926ac42a
arturovzla93 <68382596+arturovzla93@users.noreply.github.com>
2020-11-12 15:02:01 +0000

    Add files via upload

---
+++ Magazine/US.js
@@ -0,0 +1,25 @@
+const main = document.querySelector('main');
+
+window.addEventListener('load', () =>{
+    updateNews();
+});
+
+
+const updateNews = () => {
+    const url = `http://newsapi.org/v2/everything?domains=wsj.com&apiKey=489383a1f0a647bc876947861625fa60`;
+    
+    fetch(url)
+    .then(res => res.json())
+    .then(data => main.innerHTML = data.articles.map(createArticle).join('\n'));
+}
+
+const createArticle = article => {
+    return ` 
+    <div class="article"
+        <a href="${article.url}">
+            <h2>${article.title}</h2>
+            <img src="${article.urlToImage}">
+            <p>${article.description}</p>
+        </a>
+    </div>`;
+}
